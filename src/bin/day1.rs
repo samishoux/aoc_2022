1f60c1f897b26cfd986d7aa83c5b700c8261c5b5
samishoux <samuel.chabot@live.ca>
2022-12-01 16:02:48 -0500

    better day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,23 +1,24 @@
 fn main() {
     let file = include_str!("./input1.txt");
 
-    elf_calorie_iterator(file);
+    let parsed_lines = elf_calorie_iterator(file);
+
+    println!("Part 1: {:?}", parsed_lines.first().unwrap());
+
+    println!("Part 2: {:?}", parsed_lines.iter().take(3).sum::<i32>());
 }
 
-fn elf_calorie_iterator(input: &str) {
+fn elf_calorie_iterator(input: &str) -> Vec<i32> {
     let lines = input.split("\n\n");
 
     let mut parsed_lines: Vec<i32> = lines
         .map(|line| {
             line.split("\n")
-                .map(|single_calory| single_calory.parse::<i32>().unwrap())
+                .flat_map(|single_calory| single_calory.parse::<i32>())
                 .sum()
         })
         .collect();
 
     parsed_lines.sort_by(|a, b| b.cmp(a));
-
-    println!("Part 1: {:?}", parsed_lines.first().unwrap());
-
-    println!("Part 2: {:?}", parsed_lines.iter().take(3).sum::<i32>());
+    return parsed_lines;
 }
